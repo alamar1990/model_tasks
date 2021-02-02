06b2d4a8f56844c134d79917508790e37b242fe5
Alain Marquez <alamar1990@gmail.com>
2021-02-02 16:21:54 -0500

    Update models

--- src/modules/models.js
+++ src/modules/models.js
@@ -3,3 +3,6 @@
 */
 
 exports.Model = require('./model/Model');
+exports.Section = require('./sections/Section');
+exports.Task = require('./tasks/Task');
+exports.Objective = require('./objectives/Objective');
