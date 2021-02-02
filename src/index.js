92780a7eb23679ff8630d424c18520ed55b45d41
Alain Marquez <alamar1990@gmail.com>
2021-02-02 11:38:09 -0500

    Setup app initialization into mongoDb connection callback

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
+const mongoose = require('mongoose');
 const application = require('./application');
 const {config} = require('./configs/config');
 
 let server;
 
-server = application.listen(config.app.port, () => {
-    console.log(`Listening to port ${config.app.port}`);
+const mongooseOptions = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}
+
+mongoose.connect(`mongodb://${config.db.host}:${config.db.port}/${config.db.name}`, mongooseOptions).then(() => {
+    console.log('Connected to MongoDB');
+    server = application.listen(config.app.port, () => {
+        console.log(`Listening to port ${config.app.port}`);
+    });
 });
 
 //Capture the terminal
