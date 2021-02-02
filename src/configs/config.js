8233775e0615675d894cccefea0b6d1bf5cd643a
Alain Marquez <alamar1990@gmail.com>
2021-02-02 11:37:09 -0500

    Setup mongoose and mongoDB collection in config

--- src/configs/config.js
+++ src/configs/config.js
@@ -3,12 +3,22 @@ const env = process.env.NODE_ENV;
 const dev = {
     app: {
         port: parseInt(process.env.DEV_APP_PORT) || 3000
+    },
+    db: {
+        host: process.env.DEV_DB_HOST || 'localhost',
+        port: parseInt(process.env.DEV_DB_PORT) || 27017,
+        name: process.env.DEV_DB_NAME || 'dofleini_models_task'
     }
 };
 
 const test = {
     app: {
         port: parseInt(process.env.TEST_APP_PORT) || 3000
+    },
+    db: {
+        host: process.env.TEST_DB_HOST || 'localhost',
+        port: parseInt(process.env.TEST_DB_PORT) || 27017,
+        name: process.env.TEST_DB_NAME || 'dofleini_models_task'
     }
 };
 
