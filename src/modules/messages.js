c6564cea44d6086080aa261a2ba7bc73a32a7d07
Alain Marquez <alamar1990@gmail.com>
2021-02-03 15:38:51 -0500

    Centralize all error messages

---
+++ src/modules/messages.js
@@ -0,0 +1,9 @@
+module.exports = {
+    error: {
+        server: 'Error de servidor',
+        create: 'No se pudo crear',
+        update: 'No se pudo actualizar',
+        delete: 'No se pudo eliminar',
+    },
+    success: {}
+};
